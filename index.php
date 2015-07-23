8366041e85c16522fbcc8ccafcee6f0a6d4dba60
Your Name <you@example.com>
2015-07-23 09:14:04 -0400

    First

---
+++ index.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>My Bedstuy :: Coming Soon</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand logo" href="#">>My Bedstuy</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li class="dropdown-header">Nav header</li>
+            <li><a href="#">Separated link</a></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Default</a></li>
+        <li><a href="#">Static top</a></li>
+        <li><a href="#">Fixed top <span class="sr-only">(current)</span></a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+
+  <div class="jumbotron">
+    <div class="container">
+      <h2>Serving Bedstuy and the surrounding Area</h2>
+    </div>
+  </div>
+
+    <main class="main container">
+       <div class="row">
+          <div class="col-sm-6 left-col">
+            
+            <article class="post">
+              <header>
+                <time datetime="">11 Arpil 2014</time>
+                <h2>The Black Swan Open Again After Fire</h2>
+                <h3>Sub title would be here</h3>
+              </header>
+              <figure>
+                <img src="http://images.nymag.com/images/2/daily/2010/04/20100422_black_1_560x375.jpg" class="img-responsive">
+                <figcaption>Caption to photo would be here</figcaption>
+              </figure>
+              <section>
+                <p>Aesthetic Neutra photo booth Godard, pork belly Carles Bushwick pickled banh mi lumbersexual food truck. Asymmetrical pork belly master cleanse Odd Future, actually Bushwick crucifix mustache banh mi small batch put a bird on it. Lomo High Life slow-carb gastropub Godard fanny pack. Williamsburg cold-pressed art party umami, before they sold out synth selvage. Taxidermy gluten-free kitsch craft beer 8-bit mumblecore. Shoreditch street art Etsy quinoa. Cray direct trade sartorial, fixie small batch keytar Shoreditch High Life literally.</p>
+                <a href="" class="fancylink">Read More</a>
+              </section>
+            </article>
+            <article class="post">
+              <header>
+                <time datetime="">11 Arpil 2014</time>
+                <h2>The Black Swan Open Again After Fire</h2>
+                <h3>Sub title would be here</h3>
+              </header>
+              <figure>
+                <img src="http://images.nymag.com/images/2/daily/2010/04/20100422_black_1_560x375.jpg" class="img-responsive">
+                <figcaption>Caption to photo would be here</figcaption>
+              </figure>
+              <section>
+                <p>Aesthetic Neutra photo booth Godard, pork belly Carles Bushwick pickled banh mi lumbersexual food truck. Asymmetrical pork belly master cleanse Odd Future, actually Bushwick crucifix mustache banh mi small batch put a bird on it. Lomo High Life slow-carb gastropub Godard fanny pack. Williamsburg cold-pressed art party umami, before they sold out synth selvage. Taxidermy gluten-free kitsch craft beer 8-bit mumblecore. Shoreditch street art Etsy quinoa. Cray direct trade sartorial, fixie small batch keytar Shoreditch High Life literally.</p>
+                <a href="">Read More</a>
+              </section>
+            </article>
+            
+            
+                        
+          </div>
+          <div class="col-sm-6 right-col">
+                <div class="row">
+                  
+                    <div class="col-sm-7 text-center pull-right">
+                      <img src="http://placehold.it/300x600" class="img-responsive">
+                    </div>
+                    
+                    
+                    <div class="col-sm-5 pull-left">
+                      
+                            <article class="post">
+                              <header>
+                                <time datetime="">11 Arpil 2014</time>
+                                <h3>The Black Swan Open Again After Fire</h3>
+                                <h4>Sub title would be here</h4>
+                              </header>
+                              <figure>
+                                <img src="http://images.nymag.com/images/2/daily/2010/04/20100422_black_1_560x375.jpg" class="img-responsive">
+                                <figcaption>Caption to photo would be here</figcaption>
+                              </figure>
+                              <section>
+                                <p>Aesthetic Neutra photo booth Godard, pork belly Carles Bushwick pickled banh mi lumbersexual food truck. Asymmetrical...</p>
+                                <a href="">Read More</a>
+                              </section>
+                            </article>
+                            <article class="post">
+                              <header>
+                                <time datetime="">11 Arpil 2014</time>
+                                <h3>The Black Swan Open Again After Fire</h3>
+                                <h4>Sub title would be here</h4>
+                              </header>
+                              <figure>
+                                <img src="http://images.nymag.com/images/2/daily/2010/04/20100422_black_1_560x375.jpg" class="img-responsive">
+                                <figcaption>Caption to photo would be here</figcaption>
+                              </figure>
+                              <section>
+                                <p>Aesthetic Neutra photo booth Godard, pork belly Carles Bushwick pickled banh mi lumbersexual food truck. Asymmetrical pork belly master ...</p>
+                                <a href="">Read More</a>
+                              </section>
+                            </article>                      
+                      
+                    </div>
+          
+                </div>
+          </div>
+       </div>
+    </main>
+    
+    
+    <footer class="main-footer container-fluid">
+      <section class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <ul>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+                <li><a href="">Item</a></li>
+              </ul>
+            </div>
+          </div>
+      </section>
+    </footer>
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+</body>
+</html>
